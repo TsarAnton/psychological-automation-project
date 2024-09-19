a1990feb6f42703851f696c124d4f28270db4941
KapToxa <tsarenkoantoha@mail.ru>
2024-09-19 10:02:50 +0300

    feat-0006: add controllers to all entities in test module

---
+++ src/test/controllers/indicator.controller.ts
@@ -0,0 +1,64 @@
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Post, Put, Query } from "@nestjs/common";
+import { DataSource } from "typeorm";
+
+import { Indicator } from "../entities/indicator.entity";
+import { IndicatorService } from "../services/indicator.service";
+import { CreateIndicatorDto, ReadAllIndicatorsDto, UpdateIndicatorDto } from "../dto/indicator.dto";
+import { ReadAllResult } from "src/common/types/read-all-result.types";
+import { BaseController } from "src/common/classes/base-controller";
+
+@Controller('indicators')
+export class IndicatorController extends BaseController {
+    constructor(
+        protected dataSource: DataSource,
+        private indicatorService: IndicatorService,
+    ) {
+        super(dataSource);
+    }
+
+    @Get()
+    @HttpCode(HttpStatus.OK)
+    public async getAllAction(
+        @Query() readAllIndicatorsDto: ReadAllIndicatorsDto,
+    ): Promise<ReadAllResult<Indicator>> {
+        const { pagination, sorting, ...filter } = readAllIndicatorsDto;
+        return this.indicatorService.readAll({
+            pagination,
+            sorting,
+            filter,
+        });
+    }
+
+    @Get(':id')
+    @HttpCode(HttpStatus.OK)
+    public async getOneAction(
+        @Param('id') id: number,
+    ): Promise<Indicator> {
+        return await this.indicatorService.readById(id);
+    }
+
+    @Post()
+    @HttpCode(HttpStatus.OK)
+    public async createAction(
+        @Body() createIndicatorDto: CreateIndicatorDto,
+    ): Promise<Indicator> {
+        return this.indicatorService.create(createIndicatorDto);
+    }
+
+    @Put(':id')
+    @HttpCode(HttpStatus.OK)
+    public async updateAction(
+        @Param('id') id: number,
+        @Body() updateIndicatorDto: UpdateIndicatorDto,
+    ): Promise<Indicator> {
+        return this.indicatorService.update(id, updateIndicatorDto);
+    }
+
+    @Delete(':id')
+    @HttpCode(HttpStatus.NO_CONTENT)
+    public async deleteAction(
+        @Param('id') id: number,
+    ): Promise<void> {
+        return this.indicatorService.delete(id);
+    }
+}
---
+++ src/test/controllers/result.controller.ts
@@ -0,0 +1,72 @@
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Post, Put, Query } from "@nestjs/common";
+import { DataSource } from "typeorm";
+
+import { Result } from "../entities/result.entity";
+import { ResultService } from "../services/result.service";
+import { CreateResultDto, ReadAllResultsDto, ReadFullResultsDto, UpdateResultDto } from "../dto/result.dto";
+import { ReadAllResult } from "src/common/types/read-all-result.types";
+import { BaseController } from "src/common/classes/base-controller";
+
+@Controller('results')
+export class ResultController extends BaseController {
+    constructor(
+        protected dataSource: DataSource,
+        private resultService: ResultService,
+    ) {
+        super(dataSource);
+    }
+
+    @Get()
+    @HttpCode(HttpStatus.OK)
+    public async getAllAction(
+        @Query() readAllResultsDto: ReadAllResultsDto,
+    ): Promise<ReadAllResult<Result>> {
+        const { pagination, sorting, ...filter } = readAllResultsDto;
+        return this.resultService.readAll({
+            pagination,
+            sorting,
+            filter,
+        });
+    }
+
+    @Get('/full')
+    @HttpCode(HttpStatus.OK)
+    public async getOneFullAction(
+        @Query() readFullResultDto: ReadFullResultsDto,
+    ): Promise<Result> {
+        return this.resultService.readFullById(readFullResultDto);
+    }
+
+    @Get(':id')
+    @HttpCode(HttpStatus.OK)
+    public async getOneAction(
+        @Param('id') id: number,
+    ): Promise<Result> {
+        return await this.resultService.readById(id);
+    }
+
+    @Post()
+    @HttpCode(HttpStatus.OK)
+    public async createAction(
+        @Body() createResultDto: CreateResultDto,
+    ): Promise<Result> {
+        return this.resultService.create(createResultDto);
+    }
+
+    @Put(':id')
+    @HttpCode(HttpStatus.OK)
+    public async updateAction(
+        @Param('id') id: number,
+        @Body() updateResultDto: UpdateResultDto,
+    ): Promise<Result> {
+        return this.resultService.update(id, updateResultDto);
+    }
+
+    @Delete(':id')
+    @HttpCode(HttpStatus.NO_CONTENT)
+    public async deleteAction(
+        @Param('id') id: number,
+    ): Promise<void> {
+        return this.resultService.delete(id);
+    }
+}
